47468f3bce02d116060121fb54f6a5708934ec7a
siberiankikorik <stelthman@yandex.ru>
2022-02-10 20:01:37 +0700

    print and change bytes via binary operations and via pointer

--- bytes.c
+++ bytes.c
@@ -7,19 +7,32 @@ int main()
     int b = a;
     int c;
     int i;
+    void* ptr = &a;
     unsigned char buf;
     
+    // вывод по байтам
     printf("hex form of 123456789\n%x\n",a);
-    
     for (i = 0; i < 4; i++){
         buf = b & 0xFF;
         printf("byte %d is %x\n", i, buf);
         b = b >> 8;
     }
     
+    // замена байта
     c = a & 0xFF00FFFF;
     c = c | 0xDD0000;
     printf("byte 2 changed to DD\n%x\n",c);
+    
+    // вывод через указатель для little-endian памяти
+    for (i = 0; i < 4; i++){
+        buf = *( (char*)ptr + i);
+        printf("byte %d is %x\n", i, buf);
+    }
+    
+    // замена байта по указателю
+    *( (unsigned char*)ptr + 2) = 0xDD;
+    printf("byte 2 changed to DD\n%x\n",c);
+    
     
     return 0;
 }
